fe9be54c548ce995fde14850a9e70801b1e05d03
Anil Bridgpal <bridgpal@gmail.com>
2014-07-14 12:23:26 -0700

    Add simple solution

---
+++ app.js
@@ -0,0 +1,26 @@
+var marker = function() {
+  return moves % 2 === 0 ? "X" : "O"
+}
+
+var clearBoard = function () {
+  for(var i=0; i<el.length; i++) {
+    el[i].innerHTML="";
+  }
+}
+
+var moves = 0;
+var el = document.getElementsByClassName('box');
+
+window.onload = function() {
+  for (var i=0;i<el.length; i++) {
+      el[i].onclick = function(event){
+
+        if (!this.innerHTML) {
+          this.innerHTML= marker() ;
+          this.classList.add(marker())
+          moves++;
+        }
+
+      };
+  }
+}
